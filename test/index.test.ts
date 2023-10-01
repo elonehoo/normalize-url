626bea5cfaf82d20274f605cfcb1a451b77b068e
elonehoo <hi@elonehoo.me>
2023-10-01 14:23:06 +0800

    chore: update dept

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,12 @@
 import { expect, it } from 'vitest'
 import normalizeUrl from '../src/index'
 
-it('main',()=>{
+it('main', () => {
   expect(normalizeUrl('elonehoo.xyz')).toBe('http://elonehoo.xyz')
   expect(normalizeUrl('elonehoo.xyz ')).toBe('http://elonehoo.xyz')
   expect(normalizeUrl('elonehoo.xyz.')).toBe('http://elonehoo.xyz')
   expect(normalizeUrl('Elonehoo.xyz')).toBe('http://elonehoo.xyz')
-  expect(normalizeUrl('elonehoo.xyz',{defaultProtocol:'https:'})).toBe('https://elonehoo.xyz')
+  expect(normalizeUrl('elonehoo.xyz', { defaultProtocol: 'https:' })).toBe('https://elonehoo.xyz')
   expect(normalizeUrl('HTTP://elonehoo.xyz')).toBe('http://elonehoo.xyz')
   expect(normalizeUrl('//elonehoo.xyz')).toBe('http://elonehoo.xyz')
   expect(normalizeUrl('http://elonehoo.xyz')).toBe('http://elonehoo.xyz')
@@ -30,11 +30,11 @@ it('main',()=>{
   expect(normalizeUrl('http://elonehoo.xyz/?b=bar&a=foo')).toBe('http://elonehoo.xyz/?a=foo&b=bar')
   expect(normalizeUrl('http://elonehoo.xyz/?foo=bar*|<>:"')).toBe('http://elonehoo.xyz/?foo=bar*|%3C%3E:%22')
   expect(normalizeUrl('http://elonehoo.xyz:5000')).toBe('http://elonehoo.xyz:5000')
-  expect(normalizeUrl('//elonehoo.xyz/', {normalizeProtocol: false})).toBe('//elonehoo.xyz')
-  expect(normalizeUrl('//elonehoo.xyz:80/', {normalizeProtocol: false})).toBe('//elonehoo.xyz')
+  expect(normalizeUrl('//elonehoo.xyz/', { normalizeProtocol: false })).toBe('//elonehoo.xyz')
+  expect(normalizeUrl('//elonehoo.xyz:80/', { normalizeProtocol: false })).toBe('//elonehoo.xyz')
   expect(normalizeUrl('http://elonehoo.xyz/foo#bar')).toBe('http://elonehoo.xyz/foo#bar')
-  expect(normalizeUrl('http://elonehoo.xyz/foo#bar', {stripHash: true})).toBe('http://elonehoo.xyz/foo')
-  expect(normalizeUrl('http://elonehoo.xyz/foo#bar:~:text=hello%20world', {stripHash: true})).toBe('http://elonehoo.xyz/foo')
+  expect(normalizeUrl('http://elonehoo.xyz/foo#bar', { stripHash: true })).toBe('http://elonehoo.xyz/foo')
+  expect(normalizeUrl('http://elonehoo.xyz/foo#bar:~:text=hello%20world', { stripHash: true })).toBe('http://elonehoo.xyz/foo')
   expect(normalizeUrl('http://elonehoo.xyz/foo/bar/../baz')).toBe('http://elonehoo.xyz/foo/baz')
   expect(normalizeUrl('http://elonehoo.xyz/foo/bar/./baz')).toBe('http://elonehoo.xyz/foo/bar/baz')
   expect(normalizeUrl('elonehoo://www.elonehoo.com')).toBe('elonehoo://elonehoo.com')
@@ -43,7 +43,7 @@ it('main',()=>{
   expect(normalizeUrl('https://i.vimeocdn.com/filter/overlay?src0=https://i.vimeocdn.com/video/598160082_1280x720.jpg&src1=https://f.vimeocdn.com/images_v6/share/play_icon_overlay.png')).toBe('https://i.vimeocdn.com/filter/overlay?src0=https://i.vimeocdn.com/video/598160082_1280x720.jpg&src1=https://f.vimeocdn.com/images_v6/share/play_icon_overlay.png')
 })
 
-it('stripAuthentication option',()=>{
+it('stripAuthentication option', () => {
   expect(normalizeUrl('http://user:password@www.elonehoo.xyz')).toBe('http://elonehoo.xyz')
   expect(normalizeUrl('https://user:password@www.elonehoo.xyz')).toBe('https://elonehoo.xyz')
   expect(normalizeUrl('https://user:password@www.elonehoo.xyz/@user')).toBe('https://elonehoo.xyz/@user')
@@ -51,18 +51,18 @@ it('stripAuthentication option',()=>{
   expect(normalizeUrl('http://user:password@www.êxample.com')).toBe('http://xn--xample-hva.com')
   expect(normalizeUrl('elonehoo://user:password@www.elonehoo.com')).toBe('elonehoo://elonehoo.com')
 
-  const options = {stripAuthentication: false}
+  const options = { stripAuthentication: false }
 
-  expect(normalizeUrl('http://user:password@www.elonehooelonehoo.com',options)).toBe('http://user:password@elonehooelonehoo.com')
-  expect(normalizeUrl('https://user:password@www.elonehoo.com',options)).toBe('https://user:password@elonehoo.com')
-  expect(normalizeUrl('https://user:password@www.elonehoo.com/@user',options)).toBe('https://user:password@elonehoo.com/@user')
-  expect(normalizeUrl('user:password@elonehoo.com',options)).toBe('http://user:password@elonehoo.com')
-  expect(normalizeUrl('http://user:password@www.êxample.com',options)).toBe('http://user:password@xn--xample-hva.com')
-  expect(normalizeUrl('elonehoo://user:password@www.elonehoo.com',options)).toBe('elonehoo://user:password@elonehoo.com')
+  expect(normalizeUrl('http://user:password@www.elonehooelonehoo.com', options)).toBe('http://user:password@elonehooelonehoo.com')
+  expect(normalizeUrl('https://user:password@www.elonehoo.com', options)).toBe('https://user:password@elonehoo.com')
+  expect(normalizeUrl('https://user:password@www.elonehoo.com/@user', options)).toBe('https://user:password@elonehoo.com/@user')
+  expect(normalizeUrl('user:password@elonehoo.com', options)).toBe('http://user:password@elonehoo.com')
+  expect(normalizeUrl('http://user:password@www.êxample.com', options)).toBe('http://user:password@xn--xample-hva.com')
+  expect(normalizeUrl('elonehoo://user:password@www.elonehoo.com', options)).toBe('elonehoo://user:password@elonehoo.com')
 })
 
-it('stripProtocol option',()=>{
-  const options = {stripProtocol: true}
+it('stripProtocol option', () => {
+  const options = { stripProtocol: true }
   expect(normalizeUrl('http://www.elonehoo.com', options)).toBe('elonehoo.com')
   expect(normalizeUrl('http://elonehoo.com', options)).toBe('elonehoo.com')
   expect(normalizeUrl('https://www.elonehoo.com', options)).toBe('elonehoo.com')
